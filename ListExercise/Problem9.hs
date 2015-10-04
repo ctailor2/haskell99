d81fecf50495edc0fd1e399f05419afbb0bba621
Chirag Tailor <ctailor2@gmail.com>
2015-10-04 10:26:43 -0500

    exercise 9 - no working solution, need to revisit

---
+++ ListExercise/Problem9.hs
@@ -0,0 +1,14 @@
+module ListExercise.Problem9 where
+-- Pack consecutive duplicates of list elements into sublists.
+-- If a list contains repeated elements, they should be placed in separate sublists
+
+-- This sequence makes sense in my head, but this isn't compiling
+pack :: [a] -> [[a]]
+pack [] = []
+pack (x:xs) = pack_acc xs [[x]]
+        where
+              pack_acc [] packed_list = packed_list
+              pack_acc (y:ys) packed_list
+                | y == (last (last packed_list))  = pack_acc ys ((init packed_list) ++ ((last packed_list) ++ y))
+                | otherwise                       = pack_acc ys (packed_list ++ [y])
+
