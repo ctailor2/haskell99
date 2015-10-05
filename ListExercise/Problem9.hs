4b72be674bf6330430e4fe0d1ffaf1b521050454
Chirag Tailor <ctailor2@gmail.com>
2015-10-05 13:38:17 -0500

    Add listed solution for exercise 9

--- ListExercise/Problem9.hs
+++ ListExercise/Problem9.hs
@@ -3,6 +3,7 @@ module ListExercise.Problem9 where
 -- If a list contains repeated elements, they should be placed in separate sublists
 
 -- This sequence makes sense in my head, but this isn't compiling
+{-
 pack :: [a] -> [[a]]
 pack [] = []
 pack (x:xs) = pack_acc xs [[x]]
@@ -11,4 +12,14 @@ pack (x:xs) = pack_acc xs [[x]]
               pack_acc (y:ys) packed_list
                 | y == (last (last packed_list))  = pack_acc ys ((init packed_list) ++ ((last packed_list) ++ y))
                 | otherwise                       = pack_acc ys (packed_list ++ [y])
+-}
+
+-- Listed solution
+
+pack :: (Eq a) => [a] -> [[a]]
+pack [] = []
+pack [x] = [[x]]
+pack (x:xs) = if x `elem` (head (pack xs))
+              then (x:(head (pack xs))):(tail (pack xs))
+              else [x]:(pack xs)
 
